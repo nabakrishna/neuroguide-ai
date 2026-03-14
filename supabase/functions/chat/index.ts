c5cf74358eb33953634e8fd34cb20d789b375258
NABA KRISHNA HAZARIKA <nabakrishna0111@gmail.com>
2026-03-14 13:47:45 +0530

    Update index.ts

--- supabase/functions/chat/index.ts
+++ supabase/functions/chat/index.ts
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-//import { createinstantServer } from ""http
+//import { createinstantServer } from "https://esm.sh/@supabse/supabase-js@3";
 
 
 const corsHeaders = {
