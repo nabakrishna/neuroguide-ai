b764bb3f9694fd9c4f3a6e25d2e9d4b070a670ce
NABA KRISHNA HAZARIKA <nabakrishna0111@gmail.com>
2026-03-17 22:46:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
-
+// import { componentTagger } from "lovable-tagger"; for now  the following code 6 to 12 work this instead fo this
+//------------------
+import { componentTagger } from '@nuxie/tagger';
+export default defineConfig({
+  plugins: [
+    react(),
+    mode === 'development' && componentTagger(),
+  ].filter(Boolean),
+});
+//---------
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
