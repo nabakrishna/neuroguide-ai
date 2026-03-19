414386eb439cc2369e12c0ceb854312b861fc676
NABA KRISHNA HAZARIKA <nabakrishna0111@gmail.com>
2026-03-19 22:17:07 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,6 @@ export default tseslint.config(
   { ignores: ["dist"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
